18754ff48e9ed95fa5e8b210473a195dbaac4c3b
erxes <dev@erxes.io>
2026-06-24 10:50:30 +0800

    chore: initial site build

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 const nextConfig = {
   output: "export",
   distDir: "dist",
+  basePath: "/marmansu-llc",
   images: {
     unoptimized: true,
   },
