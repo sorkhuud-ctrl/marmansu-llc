5bbe84bf057c4c809b0f3056d7d6775ddda990c4
erxes <dev@erxes.io>
2026-06-24 10:49:07 +0800

    Deploy to GitHub Pages

---
+++ _next/static/biwFnDgOEdfvI9q553gE8/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fcontact","\u002F[locale]\u002Fnews","\u002F[locale]\u002Fpartners","\u002F[locale]\u002Fproducts","\u002F[locale]\u002Fprojects","\u002F[locale]\u002Fservices"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
